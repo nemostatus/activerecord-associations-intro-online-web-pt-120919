c981d80af8f4f84e94649bc3b2834debe89652e9
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-03-08 22:09:37 +0000

    Automatically backed up by Learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,6 +1,6 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
   def change 
-    create_Tabel :songs do |t|
+    create_Table :songs do |t|
       t.string :name 
       t.integer :artist_id 
       t.integer :genre_id
